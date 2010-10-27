7e7f27c2ce82c32b9b4f045e16b1dedea67530ce
Ian Asaff <ian.asaff@gmail.com>
2010-10-27 12:40:39 -0400

    generated users controller and model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def title
+    base_title = "KeepTrackOfThis.com"
+    @title.nil? ? base_title : "#{base_title} | #{@title}"
+  end
+  
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,7 +1,2 @@
 module PagesHelper
-
-  def title
-    base_title = "KeepTrackOfThis.com"
-    @title.nil? ? base_title : "#{base_title} | #{@title}"
-  end
 end
