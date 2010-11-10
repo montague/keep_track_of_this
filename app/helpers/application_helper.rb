b60e613e65047433ebb2614e4c060c08b736f9e4
Ian Asaff <ian.asaff@gmail.com>
2010-11-10 06:09:07 -0500

    finished sign in/out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,5 +4,9 @@ module ApplicationHelper
     base_title = "KeepTrackOfThis.com"
     @title.nil? ? base_title : "#{base_title} | #{@title}"
   end
-  
+
+  def logo
+    image_tag("logo.jpg", :alt => "Sample App", :class => "round")
+  end
+
 end
