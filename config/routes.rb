4f1f8bcf34a548f038b081e993014eee77f2ed37
Ian Asaff <ian.asaff@gmail.com>
2010-11-10 06:14:20 -0500

    activated footer links

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ KeepTrackOfThis::Application.routes.draw do
   match '/signin', :to => 'sessions#new'
   match '/signout', :to => 'sessions#destroy'
 
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
