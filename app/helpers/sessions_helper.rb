0f55f8345142eb6f556e59f38d55d8f48ceb3410
Ian Asaff <ian.asaff@gmail.com>
2010-11-17 07:01:42 -0500

    added option for session management via session variable, refactored users_spec.rb

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,32 +1,45 @@
 module SessionsHelper
+  USE_SESSION = true
   def sign_in(u)
-    cookies.permanent.signed[:remember_token] = [u.id, u.salt]
+    if USE_SESSION
+      session[:remember_token] = [u.id, u.salt]
+    else
+      cookies.permanent.signed[:remember_token] = [u.id, u.salt]
+    end
     current_user = u
   end
-  
+
   def sign_out
-    cookies.delete(:remember_token)
+    if USE_SESSION
+      session[:remember_token] = nil
+    else
+      cookies.delete(:remember_token)  
+    end    
     current_user = nil
   end
-  
+
   def current_user=(u)
     @current_user = u
   end
-  
+
   def current_user
     @current_user ||= user_from_remember_token
   end
-  
+
   def signed_in?
     !current_user.nil?
   end
-  
+
   private
-    def user_from_remember_token
-      User.authenticate_with_salt(*remember_token)
-    end
-    
-    def remember_token
+  def user_from_remember_token
+    User.authenticate_with_salt(*remember_token)
+  end
+
+  def remember_token
+    if USE_SESSION
+      session[:remember_token] || [nil,nil]
+    else
       cookies.signed[:remember_token] || [nil, nil]
     end
+  end
 end
