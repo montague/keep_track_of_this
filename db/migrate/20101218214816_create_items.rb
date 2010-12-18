af695cfd119887b86a86d772edf4d65ed7a4b040
Ian Asaff <ian.asaff@gmail.com>
2010-12-18 17:14:48 -0500

    initial add of items and tags

---
+++ db/migrate/20101218214816_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration
+  def self.up
+    create_table :items do |t|
+      t.string :content
+      t.integer :user_id
+
+      t.timestamps
+    end
+    add_index :items, :user_id
+  end
+
+  def self.down
+    drop_table :items
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101213112523) do
+ActiveRecord::Schema.define(:version => 20101218214853) do
+
+  create_table "items", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "items", ["user_id"], :name => "index_items_on_user_id"
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
